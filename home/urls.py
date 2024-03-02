c6566780a44d726532972369c9bb5b26e828b447
Ali Saranj <ali@smileitsolutions.uk>
2024-03-03 00:09:58 +0330

    add contact.html

--- home/urls.py
+++ home/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('about', views.about, name="about"),
     path('plans', views.plans, name="plans"),
+    path('contact', views.ProjectCreateView.as_view(), name="contact"),
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
