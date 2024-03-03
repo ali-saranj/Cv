17dc4b5f2a4971265870be9f61e8ce1e6c4ff68a
ali saranj <78583703+ali-saranj@users.noreply.github.com>
2024-03-03 16:29:06 +0330

    Revert "Done"

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from home.models import Plan, Project
+from home.models import Plan
 
 
 # Register your models here.
@@ -8,8 +8,3 @@ from home.models import Plan, Project
 @admin.register(Plan)
 class Admin(admin.ModelAdmin):
     list_display = ['pk', 'name']
-
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'name', 'phone', 'email', 'description']
--- home/models.py
+++ home/models.py
@@ -1,26 +1,9 @@
 from django.db import models
-from django.urls import reverse
-
 
 # Create your models here.
 
+
 class Plan(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
     image = models.ImageField(upload_to="images")
 
-    def __str__(self):
-        return f"{self.pk}_{self.name}"
-
-
-class Project(models.Model):
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=13)
-    email = models.EmailField()
-    description = models.TextField()
-
-    def get_absolute_url(self):
-        return reverse('contact')
-
-    def __str__(self):
-        return f"{self.pk}_{self.name}"
