0b87f9bfbaedecfbd05ca13db9802daef7c003ef
Narjes <narjeszamani2001@gmail.com>
2024-03-12 09:38:58 +0330

    center title and timer for slider

--- home/static/assets/js/main.js
+++ home/static/assets/js/main.js
@@ -167,7 +167,10 @@
         loop: true,
         speed: 1000,
         spaceBetween: 24,
-        autoplay:2000,
+          play:true,
+        autoplay: {
+            delay:2000
+        },
         pagination: {
           el: '.cs_pagination',
           clickable: true,
