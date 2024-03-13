c16491ef87ada560df7647816bb8f0e897afdac0
Ali Saranj <ali@smileitsolutions.uk>
2024-03-13 23:58:19 +0330

    done

--- config/settings.py
+++ config/settings.py
@@ -74,24 +74,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'root',
-#         'PASSWORD': 'M7SkYvAPacaHETMb51ij7xRw',
-#         'HOST': 'nervous-dubinsky-msnkw4af0-db',
-#         'PORT': '5432',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'root',
+        'PASSWORD': 'M7SkYvAPacaHETMb51ij7xRw',
+        'HOST': 'nervous-dubinsky-msnkw4af0-db',
+        'PORT': '5432',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
