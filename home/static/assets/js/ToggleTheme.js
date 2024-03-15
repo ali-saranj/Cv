b87c83548f3d3f3511854c5a7aa3eea51b97fe1b
Narjes <narjeszamani2001@gmail.com>
2024-03-15 15:44:00 +0330

    add Toggle Theme

--- home/static/assets/js/ToggleTheme.js
+++ home/static/assets/js/ToggleTheme.js
@@ -1,4 +1,18 @@
-function themeTogglle(){
-  let element = document.body;
-  element.classList.toggle('cs_dark')
-}
+localStorage.setItem("theme" , "light");
+const toggleThemeBtn = document.querySelectorAll("#toggle-theme");
+toggleThemeBtn.forEach(btn =>{
+    btn.addEventListener("click",function(){
+        const localTheme = localStorage.theme;
+        if (localTheme === "dark"){
+            // document.documentElement.classList.remove("dark");
+            document.querySelector("body").setAttribute("data-bs-theme", "light");
+            document.querySelector("#icon-theme").setAttribute("class", "icono-moon");
+            localStorage.theme = "light";
+        } else {
+            // document.documentElement.classList.add("dark");
+            document.querySelector("body").setAttribute("data-bs-theme", "dark");
+            document.querySelector("#icon-theme").setAttribute("class", "icono-sun");
+            localStorage.setItem("theme" , "dark");
+        }
+    })
+})
