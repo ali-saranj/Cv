d6487aa84ec1714dde8f515366db8f47ae9b4de2
NarjesZamani <narjeszamani2001@gmail.com>
2024-04-19 11:29:55 +0430

    add blogs

--- home/urls.py
+++ home/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
                   path('plans', views.plans, name="plans"),
                   path('contact', views.ProjectCreateView.as_view(), name="contact"),
                   # path('create_weblog', views.Create_weblog.as_view(), name="create_weblog"),
-                  path('get_weblog/', views.BlogView.as_view(), name='blog'),
+                  path('blog', views.BlogView.as_view(), name='blog'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                            document_root=settings.MEDIA_ROOT)
