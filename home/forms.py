a03f9c5d017bd945c2b942da93d4fc8844bf68c4
delta <amin138400138400@gmail.com>
2024-06-17 21:05:43 +0330

    edit_in_search

--- home/forms.py
+++ home/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class Search_weblog(forms.Form):
-    search_view = forms.CharField(max_length=100, required=False)
+    search_view = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'جستجو کنید...'})
+    )
