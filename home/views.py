e1dbdc08c6e1663d50c24e2ef8a3a5ef087735d9
delta <amin138400138400@gmail.com>
2024-07-29 18:31:38 +0330

    Create links

--- home/views.py
+++ home/views.py
@@ -13,135 +13,142 @@ def home(request):
     return render(request, "Public/index.html")
 
 
-def about(request):
-    return render(request, 'about.html')
-
-
-class plans(TemplateView):
-    template_name = 'Public/Portfolio.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-        p = Plan.objects.all()
-        context['p'] = p
-        return context
-
-
-def content(request):
-    return render(request, 'Public/contactUs.html')
+def plans(request):
+    return render(request, "Public/Portfolio.html")
 
 
 def blog(request):
-    search = Search_weblog(request.GET)
-    massage = ""
-
-    if search.is_valid() and search.cleaned_data.get('search_view'):
-        searchText = search.cleaned_data['search_view']
-        weblog = Post.objects.filter(title__contains=searchText)
-
-        if not weblog.exists():
-            massage = "مقاله مورد نظر پیدا نشد !"
-
-    else:
-        weblog = Post.objects.all()
-
-    new_posts = Post.objects.filter(is_featured=True)
-    context = {
-        "blogs": weblog,
-        "newPosts": new_posts,
-        "search": search,
-        "massage": massage
-    }
-    return render(request, 'Public/blogs.html', context)
+    return render(request, "Public/blogs.html")
 
 
-class is_featured(TemplateView):
-    template_name = 'weblogPage.html'
+def contactUs(request):
+    return render(request, "Public/contactUs.html")
 
-    def get_context_data(self):
-        context = super().get_context_data()
-        is_featured = Post.objects.filter(is_featured=True)
-        context['is_featured'] = is_featured
-        return context
-
-
-def blog_details(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(request, 'weblog_details.html', {'post': post})
-
-
-class ProjectCreateView(CreateView):
-    model = Project
-    fields = ['name', 'phone', 'email', 'description']
-    template_name = 'contact.html'
-
-
-# content
-def content_data(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        description = request.POST.get('description')
-
-        content = Contact(name=name, email=email, phone=phone, description=description)
-        content.save()
-        contxt = {
-            'response': "اطلاعات ذخیره شد با شما تماس خواهیم گرفت"
-        }
-
-        return render(request, 'contact.html', {'content': contxt})
-    else:
-        return HttpResponse("Method not allowed", status=405)
-
-
-def comment_post(request):
-    if request.method == 'POST':
-        name = request.POST.get('fullname')
-        email = request.POST.get('email')
-        comment = request.POST.get('comment')  # Changed 'content' to 'comment'
-        comment = Coment_Posts(name=name, email=email, content=comment)
-        comment.save()
-        context = {
-            'response': "اطلاعات ذخیره شد"
-        }
-
-        return render(request, 'weblog_details.html', {'context': context})  # Changed 'contaxt' to 'context'
-    else:
-        return HttpResponse("Method not allowed", status=405)
-
-
-# get_post_comment
-def get_comment(request):
-    comments = Coment_Posts.objects.all()
-    return render(request, 'weblog_details.html', {'comments': comments})
-
-
-# ارتباط با ما در صفحه پلن
-def Contact_us_view(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        location = request.POST.get('location')
-        add_form = Contact_us(name=name, email=email, phone=phone, location=location)
-        add_form.save()
-        context = {
-            'response': "اطلاعات ذخیره شد"
-        }
-    return render(request, 'contact_us.html')
-
-# get_blog
-# class BlogView(TemplateView):
+# class plans(TemplateView):
+#     template_name = 'Public/Portfolio.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data()
+#         p = Plan.objects.all()
+#         context['p'] = p
+#         return context
+#
+#
+# def content(request):
+#     return render(request, 'Public/contactUs.html')
+#
+#
+# def blog(request):
+#     search = Search_weblog(request.GET)
+#     massage = ""
+#
+#     if search.is_valid() and search.cleaned_data.get('search_view'):
+#         searchText = search.cleaned_data['search_view']
+#         weblog = Post.objects.filter(title__contains=searchText)
+#
+#         if not weblog.exists():
+#             massage = "مقاله مورد نظر پیدا نشد !"
+#
+#     else:
+#         weblog = Post.objects.all()
+#
+#     new_posts = Post.objects.filter(is_featured=True)
+#     context = {
+#         "blogs": weblog,
+#         "newPosts": new_posts,
+#         "search": search,
+#         "massage": massage
+#     }
+#     return render(request, 'Public/blogs.html', context)
+#
+#
+# class is_featured(TemplateView):
 #     template_name = 'weblogPage.html'
 #
 #     def get_context_data(self):
 #         context = super().get_context_data()
-#         posts = Post.objects.all()
-#         context['posts'] = posts
+#         is_featured = Post.objects.filter(is_featured=True)
+#         context['is_featured'] = is_featured
 #         return context
-
-# class Create_weblog(CreateView):
-#     model = Post
-#     fields = ['image', 'title', 'content', 'author']
+#
+#
+# def blog_details(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(request, 'weblog_details.html', {'post': post})
+#
+#
+# class ProjectCreateView(CreateView):
+#     model = Project
+#     fields = ['name', 'phone', 'email', 'description']
 #     template_name = 'contact.html'
+#
+#
+# # content
+# def content_data(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         phone = request.POST.get('phone')
+#         description = request.POST.get('description')
+#
+#         content = Contact(name=name, email=email, phone=phone, description=description)
+#         content.save()
+#         contxt = {
+#             'response': "اطلاعات ذخیره شد با شما تماس خواهیم گرفت"
+#         }
+#
+#         return render(request, 'contact.html', {'content': contxt})
+#     else:
+#         return HttpResponse("Method not allowed", status=405)
+#
+#
+# def comment_post(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('fullname')
+#         email = request.POST.get('email')
+#         comment = request.POST.get('comment')  # Changed 'content' to 'comment'
+#         comment = Coment_Posts(name=name, email=email, content=comment)
+#         comment.save()
+#         context = {
+#             'response': "اطلاعات ذخیره شد"
+#         }
+#
+#         return render(request, 'weblog_details.html', {'context': context})  # Changed 'contaxt' to 'context'
+#     else:
+#         return HttpResponse("Method not allowed", status=405)
+#
+#
+# # get_post_comment
+# def get_comment(request):
+#     comments = Coment_Posts.objects.all()
+#     return render(request, 'weblog_details.html', {'comments': comments})
+#
+#
+# # ارتباط با ما در صفحه پلن
+# def Contact_us_view(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         phone = request.POST.get('phone')
+#         location = request.POST.get('location')
+#         add_form = Contact_us(name=name, email=email, phone=phone, location=location)
+#         add_form.save()
+#         context = {
+#             'response': "اطلاعات ذخیره شد"
+#         }
+#     return render(request, 'contact_us.html')
+#
+# # get_blog
+# # class BlogView(TemplateView):
+# #     template_name = 'weblogPage.html'
+# #
+# #     def get_context_data(self):
+# #         context = super().get_context_data()
+# #         posts = Post.objects.all()
+# #         context['posts'] = posts
+# #         return context
+#
+# # class Create_weblog(CreateView):
+# #     model = Post
+# #     fields = ['image', 'title', 'content', 'author']
+# #     template_name = 'contact.html'
